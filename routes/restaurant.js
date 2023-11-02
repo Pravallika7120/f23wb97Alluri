0a71df5824d484de426af40d3fec9938b94d2e81
Pravallika7120 <pravallialluru@gmail.com>
2023-11-01 20:17:48 -0500

    Added restaurant endpoint

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('restaurant', { title: 'Search' });
+  res.render('restaurant', { title: 'Searc' });
 });
 
 module.exports = router;
